2c036ffa5a403527ded43d2643a1c071616f4a2b
Fabian Vogler <fabian@equivalence.ch>
2015-07-28 23:37:34 +0200

    Add control panel

---
+++ js/ceremonies.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+
+    var ceremoniesApp = angular.module('ceremoniesApp', []);
+
+    ceremoniesApp.controller('CeremoniesCtrl', function ($scope) {
+
+        var intercom = Intercom.getInstance();
+
+        $scope.template = 'screens/skill_callup.html';
+
+        $scope.context = {};
+        $scope.context.skill = 'Web Design'; 
+        $scope.context.results = [];
+        $scope.context.results.push({first_name: "Fredrik", last_name: "Glanrup"});
+        $scope.context.results.push({first_name: "Max", last_name: "Muster"});
+
+        $scope.typeOf = function (value) {
+            return typeof value;
+        };
+
+        $scope.update = function () {
+            intercom.emit('update', {template: $scope.template, context: $scope.context});
+        };
+
+        $scope.update();
+
+        intercom.on('poll', function () {
+            $scope.update();
+        });
+    });
+
+})();
