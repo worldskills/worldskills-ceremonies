9723e94626ace0f644bc216c61f0526ae78e1b8e
Fabian Vogler <fabian@equivalence.ch>
2015-07-28 23:37:41 +0200

    Add screen

---
+++ js/screen.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    var screenApp = angular.module('screenApp', []);
+
+    screenApp.controller('ScreenCtrl', function ($scope, $sce) {
+
+        var intercom = Intercom.getInstance();
+        intercom.on('update', function(data) {
+            $scope.$apply(function () {
+                $scope.template = data.template;
+                $scope.context = data.context;
+            });
+        });
+        intercom.emit('poll');
+    });
+
+})();
