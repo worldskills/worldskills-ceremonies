fb5eb8e20348b44515dd5151317dabf005812c99
Fabian Vogler <fabian@equivalence.ch>
2015-08-16 10:41:28 -0300

    Use live web services for WSC2015 CC

--- js/app.js
+++ js/app.js
@@ -11,11 +11,7 @@
     ceremoniesApp.constant('SCREENS', screens);
 
     ceremoniesApp.constant('WORLDSKILLS_CLIENT_ID', '88625bde16aa');
-    ceremoniesApp.constant('WORLDSKILLS_EVENT_ID', '9');
-    ceremoniesApp.constant('WORLDSKILLS_API_EVENTS', 'http://localhost:8080/events');
-    ceremoniesApp.constant('WORLDSKILLS_API_RESULTS', 'http://localhost:8080/results');
-    ceremoniesApp.constant('WORLDSKILLS_API_AUTH', 'http://localhost:8080/auth');
-    ceremoniesApp.constant('WORLDSKILLS_AUTHORIZE_URL', 'http://worldskills-auth.dev/oauth/authorize');
+    ceremoniesApp.constant('WORLDSKILLS_EVENT_ID', '10');
     ceremoniesApp.constant('WORLDSKILLS_API_EVENTS', 'https://api.worldskills.org/events');
     ceremoniesApp.constant('WORLDSKILLS_API_RESULTS', 'https://api.worldskills.org/results');
     ceremoniesApp.constant('WORLDSKILLS_API_AUTH', 'https://api.worldskills.org/auth');
