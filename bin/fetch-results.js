ff13daf6895c110e1e7db902cf4f3c719a5e8881
Fabian Vogler <fabian@equivalence.ch>
2017-11-14 17:24:30 +0100

    Use unpublished WSC2017 results

--- bin/fetch-results.js
+++ bin/fetch-results.js
@@ -6,9 +6,8 @@ var https = require('https');
 var fs = require('fs');
 var url = require('url');
 
-// var options = url.parse('https://api.worldskills.org/results/events/316?published=false');
-var options = url.parse('https://api.worldskills.org/results/events/10');
-options.headers = {authorization: 'Bearer c297529b-54fb-4004-8a70-eed0ea173405'};
+var options = url.parse('https://api.worldskills.org/results/events/316?published=false');
+options.headers = {authorization: 'Bearer 8638a25c-9755-431e-aeb9-bbbbcc13bf9f'};
 
 var file = fs.createWriteStream(__dirname + '/../data/json/results.json');
 var request = https.get(options, function(response) {
