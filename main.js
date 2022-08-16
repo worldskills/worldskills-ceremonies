02a6cc1dc347a36f834005c7182ac954d5af24f9
Fabian Vogler <fabian@equivalence.ch>
2022-08-16 17:20:57 +0200

    Hide menu bar on windows

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
+    autoHideMenuBar: true,
     width: 800,
     height: 600,
   })
