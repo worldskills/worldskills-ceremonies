a2e12fb047ce653df19db6152be4674063877c8b
Fabian Vogler <fabian@equivalence.ch>
2024-08-01 11:56:53 +0300

    Update Skills for WSC2024

--- bin/fetch-skills.js
+++ bin/fetch-skills.js
@@ -6,6 +6,6 @@ var https = require('https');
 var fs = require('fs');
 
 var file = fs.createWriteStream(__dirname + '/../data/json/skills.json');
-var request = https.get('https://api.worldskills.org/events/594/skills?sort=name_asc&l=en&limit=100&type=official', function(response) {
+var request = https.get('https://api.worldskills.org/events/579/skills?sort=name_asc&l=en&limit=100&type=official', function(response) {
     response.pipe(file);
 });
